211943e984317e57aa34a4d8c303f67ce358ed82
fihpinato <fpinatoaguiar@gmail.com>
2018-06-08 07:03:30 -0300

    First Commit

---
+++ ScriptableObjectsTest/Assets/Card/Card.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject {
+
+    public Effect effect;
+    public GameObject player;
+    public Animator playerAnim;
+
+    public void DoEffect() {
+        playerAnim.SetTrigger(effect.ToString());
+    }
+}
+
+public enum Effect {
+    Defense,
+    Attack,
+    Grab,
+    Jump
+}
---
+++ ScriptableObjectsTest/Assets/Card/CardDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CardDisplay : MonoBehaviour {
+
+    public Card card;
+
+    public TMP_Text cardNameText;
+
+    public GameObject player;
+    public Animator playerAnim;
+
+    void Start() {
+        cardNameText.text = card.effect.ToString();
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerAnim = player.GetComponent<Animator>();
+
+        card.player = player;
+        card.playerAnim = playerAnim;
+    }
+
+    private void OnMouseDown() {
+        card.DoEffect();
+    }
+}
